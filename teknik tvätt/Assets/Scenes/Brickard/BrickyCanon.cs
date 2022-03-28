8b6179bbd44331cf696ab535965f5cd62dc15e9d
te20-isafer <79262996+te20-isafer@users.noreply.github.com>
2022-03-28 14:21:20 +0200

    Changes 3
    david

---
+++ teknik tvätt/Assets/Scenes/Brickard/BrickyCanon.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickyCanon : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        if(input.mouse)
+    }
+}
